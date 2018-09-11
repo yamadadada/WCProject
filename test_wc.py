b0b51dad9f2f4f00b13627aedb227b001a4aa7ae
yamadadada <297354270@qq.com>
2018-09-12 01:12:57 +0800

    添加对通配符的识别

--- test_wc.py
+++ test_wc.py
@@ -3,7 +3,8 @@ from wc import get_chars
 from wc import get_words
 from wc import get_lines
 from wc import get_appends
-from wc import get_file_by_dir
+from wc import get_file_recursive
+from wc import get_file
 
 
 class TestGet_chars(TestCase):
@@ -27,7 +28,13 @@ class TestGet_appends(TestCase):
         get_appends(file)
 
 
-class TestGet_file_by_dir(TestCase):
-    def test_get_file_by_dir(self):
-        file_list = get_file_by_dir(r'D:\test', '')
+class TestGet_file_recursive(TestCase):
+    def test_get_file_recursive(self):
+        file_list = get_file_recursive("D:\\test", "*.cpp")
+        TestCase.assertTrue(self, len(file_list) > 0)
+
+
+class TestGet_file(TestCase):
+    def test_get_file(self):
+        file_list = get_file("D:\\test", "*.cpp")
         TestCase.assertTrue(self, len(file_list) > 0)
--- wc.py
+++ wc.py
@@ -1,5 +1,6 @@
 import argparse
 import os.path
+import re
 
 
 # 解析命令行参数
@@ -24,7 +25,28 @@ def command_parse():
         param = param + 's'
     if args.a:
         param = param + 'a'
+    # 自动补充当前路径
+    if "\\" not in file_path:
+        file_path = os.path.abspath(os.path.join(os.getcwd(), "..")) + "\\" + file_path
     return param, file_path
+
+
+def get_chars(file):
+    with open(file, 'r') as f:
+        data = f.read()
+        print("文件（" + file + "）的字符数： " + str(len(data)))
+
+
+def get_words(file):
+    with open(file, 'r') as f:
+        data = f.read()
+        print("文件（" + file + "）的词的数目： " + str(len(data.split())))
+
+
+def get_lines(file):
+    with open(file, 'r') as f:
+        data = f.read()
+        print("文件（" + file + "）的行数： " + str(len(data.split("\n"))))
 
 
 def get_appends(file):
@@ -55,84 +77,73 @@ def get_appends(file):
     print("文件（" + file + "）的注释行数： " + str(annotation))
 
 
-def get_chars(file):
-    with open(file, 'r') as f:
-        data = f.read()
-        print("文件（" + file + "）的字符数： " + str(len(data)))
-
-
-def get_words(file):
-    with open(file, 'r') as f:
-        data = f.read()
-        print("文件（" + file + "）的词的数目： " + str(len(data.split())))
-
-
-def get_lines(file):
-    with open(file, 'r') as f:
-        data = f.read()
-        print("文件（" + file + "）的行数： " + str(len(data.split("\n"))))
-
-
-def get_file_by_dir(root, suffix):
+# 通过正则表达式递归查找文件
+def get_file_recursive(root, pattern):
     file_list = []
     dir_list = os.listdir(root)
     for i in range(len(dir_list)):
         path = os.path.join(root, dir_list[i])
         if os.path.isdir(path):
-            file_list.extend(get_file_by_dir(path, suffix))
-        else:
-            if suffix == '':
-                file_list.append(path)
-            else:
-                if '.' in path and path.split('.')[1] == suffix:
-                    file_list.append(path)
+            file_list.extend(get_file_recursive(path, pattern))
+        elif os.path.isfile(path) and re.match(pattern, dir_list[i]):
+            file_list.append(path)
+    return file_list
+
+
+# 通过正则表达式不递归查找文件
+def get_file(root, pattern):
+    file_list = []
+    # 获取根路径
+    dir_list = os.listdir(root)
+    for i in range(len(dir_list)):
+        path = os.path.join(root, dir_list[i])
+        if os.path.isfile(path) and re.match(pattern, dir_list[i]):
+            file_list.append(path)
     return file_list
 
 
 def main():
     param, file_path = command_parse()
-    # 判断是否需要返回更复杂的数据
-    append = False
-    if 'a' in param:
-        append = True
     # 如果输入的是目录的路径
     if 's' in param:
-        # 如果有通配符，识别后缀
-        suffix = ''
-        if '*.' in file_path:
-            suffix = file_path.split('*.')[1]
-            file_path = file_path.split('\*.')[0]
-        # 判断路径是否是目录的路径
-        if not os.path.isdir(file_path):
-            print("输入的路径不是目录！")
-            print("ERROR: " + file_path)
-            exit()
-        # 递归遍历子目录中所有符合条件的文件
-        file_list = get_file_by_dir(file_path, suffix)
-        for file in file_list:
-            if 'c' in param:
-                get_chars(file)
-            if 'w' in param:
-                get_words(file)
-            if 'l' in param:
-                get_lines(file)
-            if 'a' in param:
-                get_appends(file)
+        # 如果有通配符,识别符合的目录,返回符合的文件路径
+        if "*" in file_path or "?" in file_path:
+            pattern = r"^" + file_path.split("\\")[-1].replace("*", "[0-9a-zA-Z]*").replace("?", "[0-9a-zA-Z]*") + "$"
+            # 获取根路径
+            root = os.path.abspath(os.path.join(file_path, ".."))
+            file_list = get_file_recursive(root, pattern)
+        # 没有通配符，先检测路径是否正确，然后递归
+        else:
+            if not os.path.isdir(file_path):
+                print("输入的路径不是目录！")
+                print("ERROR: " + file_path)
+                exit()
+            # 无通配符，匹配任意字符
+            file_list = get_file_recursive(file_path, "[\s\S]*")
     # 如果输入的是文件的路径
     else:
-        # 判断路径是否是文件的路径
-        if not os.path.isfile(file_path):
-            print("输入的路径不是文件！")
-            print("ERROR: " + file_path)
-            exit()
+        file_list = []
+        if "*" in file_path or "?" in file_path:
+            pattern = r"^" + file_path.split("\\")[-1].replace("*", "[0-9a-zA-Z]*").replace("?", "[0-9a-zA-Z]*") + "$"
+            # 获取根路径
+            root = os.path.abspath(os.path.join(file_path, ".."))
+            file_list = get_file(root, pattern)
+        else:
+            # 判断路径是否是文件的路径
+            if not os.path.isfile(file_path):
+                print("输入的路径不是文件！")
+                print("ERROR: " + file_path)
+                exit()
+            file_list.append(file_path)
+    for file in file_list:
         if 'c' in param:
-            get_chars(file_path)
+            get_chars(file)
         if 'w' in param:
-            get_words(file_path)
+            get_words(file)
         if 'l' in param:
-            get_lines(file_path)
+            get_lines(file)
         if 'a' in param:
-            get_appends(file_path)
+            get_appends(file)
 
 
 if __name__ == '__main__':
